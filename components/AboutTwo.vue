27137668d19adda1f6e296f1ca28788da2b6e8fa
DevHola <connectola@yahoo.com>
2021-07-08 14:20:40 +0100

    final commit

--- components/AboutTwo.vue
+++ components/AboutTwo.vue
@@ -10,8 +10,7 @@
            <section class="py-5 ">
                 <div class="container px-5">
                     <div class="row gx-5 align-items-center">
-                        <div class="col-lg-6 order-first order-lg-last"><h2 class="fw-bolder">Our Vision</h2>
-                            <p class="lead fw-normal text-muted mb-0">Becoming the leading Human Capital and Resources Managers in Africa..</p></div>
+                        <div class="col-lg-6 order-first order-lg-last"><img class="img-fluid rounded mb-5 mb-lg-0" src="https://dummyimage.com/600x400/343a40/6c757d" alt="..." /></div>
                         <div class="col-lg-6">
                             <h2 class="fw-bolder">Our Vision</h2>
                             <p class="lead fw-normal text-muted mb-0">Becoming the leading Human Capital and Resources Managers in Africa..</p>
